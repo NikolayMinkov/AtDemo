ceaa29a44bef90b749917bfff4a55ac1fb285306
NikolayMinkov <nikolay.minkov89@gmail.com>
2019-12-07 23:14:15 +0200

    Test

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("atDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("atDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60d2151a-f8ed-4e55-8254-706285676c7f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace atDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        
+    
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+        [TestMethod]
+        public void validname()
+        {
+            driver.Navigate().GoToUrl("https://www.abv.bg/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1073, 1040);
+            driver.FindElement(By.Id("username")).SendKeys("valid_email");
+            driver.FindElement(By.Id("password")).Click();
+            driver.FindElement(By.Id("password")).SendKeys("123456789");
+            driver.FindElement(By.Id("loginBut")).Click();
+            System.Threading.Thread.Sleep(2000);
+            Assert.AreEqual(driver.FindElement(By.Id("form.errors")).Text,"Грешен потребител / парола.");
+        }
+       
+        
+    }
+}
